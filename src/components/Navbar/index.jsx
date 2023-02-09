754b5336f726cd477d3b672907d486be2d04c389
sugiartofahmi <sugiartofahmi@gmail.com>
2023-02-09 21:14:52 +0700

    Fahmi Sugiarto | QR Generator

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const [valid, setValid] = useState(false);
 
   return (
-    <header className="pb-6 bg-white shadow-lg fixed w-full  lg:pb-0">
+    <header className=" bg-white shadow-lg fixed w-full items-center  lg:pb-0">
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <nav className="flex items-center justify-between h-16 lg:h-20">
           <div className="flex-shrink-0">
@@ -43,50 +43,23 @@ const Navbar = () => {
         {valid && (
           <nav className="pt-4 pb-6 bg-white border border-gray-200 rounded-md shadow-md lg:hidden">
             <div className="flow-root">
-              <div className="flex flex-col px-6 -my-2 space-y-1">
-                <a
-                  href="#"
-                  title=""
-                  className="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
-                >
-                  Features
-                </a>
-
-                <a
-                  href="#"
-                  title=""
-                  className="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
-                >
-                  Solutions
-                </a>
-
-                <a
-                  href="#"
-                  title=""
-                  className="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
-                >
-                  Resources
-                </a>
-
-                <a
-                  href="#"
-                  title=""
-                  className="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
-                >
-                  Pricing
-                </a>
+              <div className="flex flex-col px-6 gap-y-5 my-2 space-y-1">
+                <Link to="/">
+                  <h1 className="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
+                    Home
+                  </h1>
+                </Link>
+                <Link to="/scan">
+                  <h1 className="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
+                    Scan QR
+                  </h1>
+                </Link>
+                <Link to="/generate">
+                  <h1 className="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
+                    Generate QR
+                  </h1>
+                </Link>
               </div>
-            </div>
-
-            <div className="px-6 mt-6">
-              <a
-                href="#"
-                title=""
-                className="inline-flex justify-center px-4 py-3 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md tems-center hover:bg-blue-700 focus:bg-blue-700"
-                role="button"
-              >
-                Get started now
-              </a>
             </div>
           </nav>
         )}
--- src/pages/ScanQR/index.jsx
+++ src/pages/ScanQR/index.jsx
@@ -1,10 +1,65 @@
 import MainLayout from "../../layouts/MainLayout";
 import ContentLayout from "../../layouts/ContentLayout";
+import { BiScan } from "react-icons/bi";
+import { QrReader } from "react-qr-reader";
+import { useState, useRef } from "react";
 const ScanQR = () => {
+  const [text, setText] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [scanResultFile, setScanResultFile] = useState("");
+  const [scanResultWebCam, setScanResultWebCam] = useState("");
+
+  const qrRef = useRef(null);
+
+  const generateQrCode = async () => {
+    try {
+      const response = await QRCode.toDataURL(text);
+      setImageUrl(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleErrorFile = (error) => {
+    console.log(error);
+  };
+  const handleScanFile = (result) => {
+    if (result) {
+      setScanResultFile(result);
+    }
+  };
+  const onScanFile = () => {
+    qrRef.current.openImageDialog();
+  };
+  const handleErrorWebCam = (error) => {
+    console.log(error);
+  };
+  const handleScanWebCam = (result) => {
+    if (result) {
+      setScanResultWebCam(result);
+    }
+  };
   return (
     <MainLayout>
       <ContentLayout>
-        <h1>Scan QR</h1>
+        <div className="flex flex-col gap-y-7 justify-center items-center">
+          <BiScan onClick={onScanFile} size={70} />
+          <QrReader
+            ref={qrRef}
+            delay={300}
+            style={{ width: "100%" }}
+            onError={handleErrorFile}
+            onScan={handleScanFile}
+            legacyMode
+          />
+          <h3>Scanned Code: {scanResultFile}</h3>
+          <QrReader
+            delay={300}
+            style={{ width: "100%" }}
+            onError={handleErrorWebCam}
+            onScan={handleScanWebCam}
+          />
+          <h3>Scanned By WebCam Code: {scanResultWebCam}</h3>
+        </div>
       </ContentLayout>
     </MainLayout>
   );
