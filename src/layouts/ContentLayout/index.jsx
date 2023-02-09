5df1cea420464c280567d42a61a1aed4e5a75540
sugiartofahmi <sugiartofahmi@gmail.com>
2023-02-10 01:05:21 +0700

    Fahmi Sugiarto | Create Router

---
+++ src/layouts/ContentLayout/index.jsx
@@ -0,0 +1,8 @@
+const ContentLayout = ({ children }) => {
+  return (
+    <main className="w-full h-screen flex flex-col justify-center items-center">
+      {children}
+    </main>
+  );
+};
+export default ContentLayout;
---
+++ src/pages/ScanQR/index.jsx
@@ -0,0 +1,9 @@
+import ContentLayout from "../../layouts/ContentLayout";
+const ScanQR = () => {
+  return (
+    <ContentLayout>
+      <h1>Scan QR</h1>
+    </ContentLayout>
+  );
+};
+export default ScanQR;
